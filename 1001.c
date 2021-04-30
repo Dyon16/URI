8f06c25eb12373fa7ed94421a180b74176b51c8b
Jardel <jardel.dyonisio@hotmail.com>
2021-04-30 17:29:54 -0300

    Done

---
+++ 1001.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ 
+int main() 
+{
+ 
+    int A, B, X;
+    
+    printf("Write the number for A: ");
+    scanf("%d", &A);
+    
+    printf("Write the number for B: ");
+    scanf("%d", &B);
+    
+    X = A + B;
+    
+    printf("The result of sum is: %d\n", X);
+ 
+    return 0;
+}
