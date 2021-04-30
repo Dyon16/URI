fe076d8fadb3233f49144752dc89303df0811200
Jardel <jardel.dyonisio@hotmail.com>
2021-04-30 17:53:38 -0300

    Done

---
+++ 1003.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main()
+{
+    int A, B, S;
+
+    printf("Write the value for A: ");
+    scanf("%d", &A);
+
+    printf("Write the value for B: ");
+    scanf("%d", &B);
+
+    S = A + B;
+
+    printf("The value of some is: %d\n", S);
+}
